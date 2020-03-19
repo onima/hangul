b45f23cbe046f374d2030cae7c839441422f4168
Alexis POirier <alexispoirier@live.com>
2020-03-19 14:27:19 +0100

    Move assets in specific folder

--- window.rb
+++ window.rb
@@ -3,7 +3,7 @@ require_relative 'lib/letter'
 
 set title: 'Hangul', background: 'white', width: 1200, height: 800
 
-Image.new('south_korea_flag.png', width: 1200, height: 800, opacity: 0.2)
+Image.new('assets/images/south_korea_flag.png', width: 1200, height: 800, opacity: 0.2)
 letter = Letter.new(name: 'giyeok', romanization: 'g')
 
 update do
